578186585727fd6f3c14915f91abbc4a71fca353
Harsha Revanth Manchikanti <90206268+Harsha2306@users.noreply.github.com>
2024-06-29 20:35:57 +0530

    t7

--- frontend/src/api/UserApi.js
+++ frontend/src/api/UserApi.js
@@ -3,11 +3,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl:
-      "https://e-commerce2-hgaw84lwt-harshas-projects-8bf50264.vercel.app/",
+    baseUrl: "https://e-commerce2-one.vercel.app/",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("token");
       if (token) headers.set("Authorization", `Bearer ${token}`);
+      return headers;
     },
   }),
   endpoints: (builder) => ({
